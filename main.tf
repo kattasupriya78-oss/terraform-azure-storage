1eb143c435870e804b86c8c6e9e299d4853e8a0f
kattasupriya78-oss <kattasupriya78@gmail.com>
2026-03-18 11:08:01 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = "Canada Central"
 }
 resource "azurerm_storage_account" "storage" {
-  name                     = "demostorageazure5678"
+  name                     = "demostorageazure5678git"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
